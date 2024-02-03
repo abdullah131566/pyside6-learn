85f6dd8ce66b9499c08a6cd7200db05c0b39b153
abdullahqadeer4 <ashfaqabdullah1122@gmail.com>
2024-02-03 12:59:05 +0500

    Update layout app with size policies and stretch

--- 004 Using basic layouts/CoolWidget.py
+++ 004 Using basic layouts/CoolWidget.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget, QPushButton, QSlider, QHBoxLayout, QVBoxLayout
+from PySide6.QtWidgets import QWidget, QPushButton, QSlider, QHBoxLayout, QVBoxLayout, QSizePolicy
 
 class CoolWidget(QWidget):
   def __init__(self):
@@ -8,18 +8,20 @@ class CoolWidget(QWidget):
     button1 = QPushButton()
     button1.setText("&Press Me!")
     button1.setCheckable(True)
+    button1.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
     
     button2 = QPushButton()
     button2.setText("&Press Me Too!")
 
-    layout1 = QVBoxLayout()
-    layout1.addWidget(button1)
-    layout1.addWidget(button2)
+    layout1 = QHBoxLayout()
+    layout1.addWidget(button1, 2)
+    layout1.addWidget(button2, 1)
 
     slider1 = QSlider()
     slider1.setOrientation(Qt.Vertical)
     slider1.setMinimum(0)
     slider1.setMaximum(100)
+    slider1.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed))
 
     slider2 = QSlider()
     slider2.setOrientation(Qt.Vertical)
@@ -28,7 +30,7 @@ class CoolWidget(QWidget):
 
     layout2 = QVBoxLayout()
     layout2.addWidget(slider1)
-    layout2.addWidget(slider2)
+    layout2.addWidget(slider2, 2)
 
     layout3 = QVBoxLayout()
     layout3.addLayout(layout1)
